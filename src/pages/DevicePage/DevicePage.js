6c7c898da52d12ec48d91f3ef88d3316a45b28eb
Artem <artemzagovora@gmail.com>
2023-11-09 22:29:00 +0200

    Fixing Ghpages img

--- src/pages/DevicePage/DevicePage.js
+++ src/pages/DevicePage/DevicePage.js
@@ -40,7 +40,7 @@ function DevicePage(){
                             dispatch(addToCart({...device, count: 1}))
                             dispatch(setVisibleCart(true))
                             }}>Купити в один клік</MyButton>
-                        <MyButton handle={()=>dispatch(addToCart({...device, count: 1}))}>У кошик</MyButton>
+                        <MyButton handle={()=>dispatch(addToCart({...device,src: device.src.slice(2), count: 1}))}>У кошик</MyButton>
                    </div>
                  </div>
                  <hr/>
